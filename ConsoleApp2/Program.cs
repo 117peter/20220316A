e918e5b0e373a651e919efe31d148e2f09d442d4
user <user@A401-PC47>
2022-03-16 16:14:11 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SCORE;
+            Console.WriteLine("請輸入你的分數");
+            SCORE = Convert.ToInt32(Console.ReadLine());
+            string RESULT = SCORE >= 60 ? "及格" : "不及格";
+            Console.WriteLine("和同學>這次你{0}"， RESULT);
+
+
+        }
+    }
+}
